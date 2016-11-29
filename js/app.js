ecdb7bf3e8ade2b0a094a55b6cc17e6c94b6ce8f
Danya <disproziy@gmail.com>
2016-11-30 01:31:57 +0300

    landing hotfix

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
- $('.carousel').carousel()
+ // Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
+
+$(".right").click(function(){
+    $("#myCarousel").carousel("next");
+});
      
 });
